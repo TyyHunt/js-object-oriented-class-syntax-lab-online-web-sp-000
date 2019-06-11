d5649c2c287ee3a5ffb17dc78d33aa5d52384c21
Tyler Hunt <tyhunt04@gmail.com>
2019-06-11 23:24:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ class BoardMember {
   };
 
   veto() { return 'No, I must disagree'};
+
+  approve() { return 'You can do it!'};
+
+  doCharity() { return 'I like to help people.'};
+
+  releasePressStatement() { return 'You will see great things from Scuber.'};
+
+  sayHi() { return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`};
 }
